e4e19d500e09e11627dd53231e012d238e5dc7a4
Koen Bolhuis <koen.bolhuis@gmail.com>
2017-06-07 21:54:40 +0200

    Mostly finished rewrite in Python

--- siteminify.py
+++ siteminify.py
@@ -4,6 +4,7 @@ import argparse
 import json
 import requests
 import re
+import shutil
 
 VERSION = '0.4'
 
@@ -11,12 +12,13 @@ CONFIG_FILE = 'config.json'
 DEFAULT_CONFIG = {
     'remove_extra_newlines': True,
     'cache_css': True,
+    'css_minify_attempts': 10,
     'pages': [],
     'other': [],
 }
 MINIFY_URL = 'https://cssminifier.com/raw'
 
-def exit_with_message(text, prefix='> Error: ', code=1):
+def exit_with_message(text, prefix='# Error: ', code=1):
     print(prefix + text)
     sys.exit(code)
 
@@ -61,9 +63,9 @@ if not os.path.exists(config_path):
         json.dump(DEFAULT_CONFIG, f, indent=4)
 
 with open(config_path, 'r') as f:
-    config = json.load(f)
+    CONFIG = json.load(f)
 
-if not config:
+if not CONFIG:
     exit_with_message('config could not be loaded')
 
 #------------
@@ -81,16 +83,70 @@ def minify(css_file_path):
     response = requests.post(MINIFY_URL, data={'input': css_content})
     return response.text
 
+def minify_and_replace(match):
+    css_file = match.group(1)
+
+    css_file_path = os.path.join(base_path, css_file)
+    css_cache_path = os.path.join(out_path, css_file + '.cache')
+
+    if os.path.exists(css_cache_path):
+        print('     * Found cached css file ' + css_file)
+        with open(css_cache_path, 'r') as f:
+            return embedded_style(f.read())
+
+    minified_css = None
+    attempts = 0
+
+    while not minified_css:
+        if attempts > CONFIG['css_minify_attempts']:
+            print('     * Minification of {} took more than {} tries. Retry? y/n'.format(css_file, CONFIG['css_minify_attempts']))
+            retry = input('     >')
+            if retry.lower() == 'y':
+                attempts = 0
+            else:
+                return ''
+
+        minified_css = minify(css_file_path)
+        attempts += 1
+
+    if CONFIG['cache_css']:
+        print('     * Caching css file ' + css_file)
+        with open(css_cache_path, 'w') as f:
+            f.write(minified_css)
+
+    return embedded_style(minified_css)
+
+tab_return_regex = re.compile(r'[\t\r]')
+newline_regex = re.compile(r'\n{2,}')
 linked_style_regex = re.compile(r'<link rel="stylesheet" href="([^"]+)" type="text/css">')
 
 def main():
     print('- Minifying pages')
 
-    for page_file_name in config['pages']:
+    for page_file_name in CONFIG['pages']:
         print('  * ' + page_file_name)
 
         with open(os.path.join(base_path, page_file_name), 'r') as f:
-            pass
+            html_content = f.read()
+
+            html_content = re.sub(tab_return_regex, '', html_content)
+
+            if CONFIG['remove_extra_newlines']:
+                html_content = re.sub(newline_regex, '\n', html_content)
+
+            html_content = re.sub(linked_style_regex, minify_and_replace, html_content)
+
+            with open(os.path.join(out_path, page_file_name), 'w') as o:
+                o.write(html_content)
+
+    print('- Copying other files')
+
+    for other_file_name in CONFIG['other']:
+        print('  * ' + other_file_name)
+
+        shutil.copyfile(os.path.join(base_path, other_file_name), os.path.join(out_path, other_file_name))
+
+    print('- Done')
 
 if __name__ == '__main__':
     main()
